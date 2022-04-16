09f60913d9616d6d9a7ede3da2d5bc5df8dfef11
James Gorodesky <jmg5369@gmail.com>
2022-04-16 11:24:10 -0600

    Finished final questions and queries

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ const db = mysql.createConnection(
 async function getAllDepartments() {
   let data = await query("SELECT * FROM department;");
   let table = cTable.getTable(data)
+  console.log("\n")
   console.log(table)
 init()
 
@@ -65,6 +66,7 @@ init()
 async function getAllRoles() {
   let data = await query("SELECT * FROM role;");
   let table = cTable.getTable(data)
+  console.log("\n")
   console.log(table)
 init()
 
@@ -72,18 +74,137 @@ init()
 async function getAllEmployees() {
   let data = await query("SELECT * FROM employee;");
   let table = cTable.getTable(data)
+  console.log("\n")
   console.log(table)
 init()
 
 }
+async function addDepartment() {
 
+    inquirer.prompt(
+      [
+        {
+          type: "input", 
+          name: "departmentName",
+          message: "What is the name of the new department?",
+          
+        }
+      ]
+    ).then((response) => {
+     insertDepartment(response)
+    })
+}
 
+async function insertDepartment(response) {
+  let data = await query(`INSERT INTO department (name) VALUES (?);`,[response.departmentName])
+getAllDepartments()
 
+  init()
+}
 
+async function addRole() {
 
+  inquirer.prompt(
+    [
+      {
+        type: "input", 
+        name: "roleName",
+        message: "What is the name of the new role?",
+        
+      },
+      {
+        type: "input", 
+        name: "roleSalary",
+        message: "What is the salary of the new role?",
+        
+      },
+      {
+        type: "input", 
+        name: "deptID",
+        message: "What is the department ID of the new role?",
+        
+      }
+    ]
+  ).then((response) => {
+   insertRole(response)
+  })
+}
 
+async function insertRole(response) {
+let data = await query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`,[response.roleName, response.roleSalary, response.deptID])
+getAllRoles()
 
+init()
+}
 
+async function addEmployee() {
 
+  inquirer.prompt(
+    [
+      {
+        type: "input", 
+        name: "employeeFirst",
+        message: "What is the first name of the new employee?",
+        
+      },
+      {
+        type: "input", 
+        name: "employeeLast",
+        message: "What is the last name of the new employee?",
+        
+      },
+      {
+        type: "input", 
+        name: "roleID",
+        message: "What is the role ID that this employee belongs to?",
+        
+      },
+      {
+        type: "input", 
+        name: "manager",
+        message: "Who is the manager of the new employee? Enter the managers ID here.",
+        
+      }
+    ]
+  ).then((response) => {
+   insertEmployee(response)
+  })
+}
+
+async function insertEmployee(response) {
+let data = await query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`,[response.employeeFirst, response.employeeLast, response.roleID, response.manager])
+getAllEmployees()
+
+init()
+}
+
+async function updateEmployee() {
+
+  inquirer.prompt(
+    [
+      {
+        type: "input", 
+        name: "employee",
+        message: "What employee would you like to update? Enter their employee ID here:",
+        
+      },
+      {
+        type: "input", 
+        name: "newRole",
+        message: "What is the new role for the employee? Enter the role ID here:",
+        
+      },
+    ]
+  ).then((response) => {
+   updateRole(response)
+  })
+}
+
+async function updateRole(response) {
+  let data = await query(`UPDATE employee SET role_id = ? WHERE id = ?;`,[response.newRole, response.employee,])
+  getAllEmployees()
+  
+  init()
+  }
 
   init()
